41edc6cd1f9565b8eeee9891c7488182cd6ee576
Florian Wagner <florian.wagner@microsoft.com>
2020-06-04 16:33:36 +0900

    Adding Shared API Key and AzureDevOps Bearer Token
    Signed-off-by: Florian Wagner <florian.wagner@microsoft.com>

---
+++ src/dotnet-core-api-shared-api-key/Controllers/WeatherForecastController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+
+namespace dotnet_core_api_shared_api_key.Controllers
+{
+    
+
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private const string api_key = "key";
+
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            if (!ValidApiKey(Request.Headers)) return Unauthorized();
+
+            var rng = new Random();
+            return CreatedAtAction(nameof(Get), 
+                Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                })
+                .ToArray()
+               );
+        }
+
+        private bool ValidApiKey(IHeaderDictionary headers)
+        {
+            headers.TryGetValue("Shared-Api-Key", out var traceValue);
+
+            return (traceValue == api_key);
+        }
+    }
+}
